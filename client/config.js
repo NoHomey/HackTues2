ae5de76251ca0357862dcc8999ba5e01d2e6e5e0
NoHomey <ivo_stratev@abv.bg>
2015-10-07 19:45:08 +0300

    Create config.js

---
+++ client/config.js
@@ -0,0 +1,14 @@
+import { controllers } from '/app.controllers.js';
+
+config.$inject = ['$routeProvider'];
+
+function config($routeProvider){
+  $routeProvider
+    .when('/', controllers.appController.config)
+    .when('/login', controllers.loginController.config)
+    .when('/register', controllers.registerController.config)
+    .otherwise({redirectTo: '/login'});
+}
+
+export var config = config;
+export default { moduleName : controllers.moduleName };
